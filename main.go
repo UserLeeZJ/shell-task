1b8490d2b15ee68509e2c02e9b698c81d4642d83
mockmoon <32706013+UserLeeZJ@users.noreply.github.com>
2025-05-14 15:52:12 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,42 @@
+// Package shelltask 提供了一个简单而灵活的任务调度系统
+package shelltask
+
+import "shell-task/scheduler"
+
+// Task 代表一个可配置的任务
+type Task = scheduler.Task
+
+// Job 定义任务函数类型
+type Job = scheduler.Job
+
+// JobResult 表示任务执行结果
+type JobResult = scheduler.JobResult
+
+// TaskOption 配置任务的函数类型
+type TaskOption = scheduler.TaskOption
+
+// New 创建新的任务实例
+func New(opts ...TaskOption) *Task {
+	return scheduler.NewTask(opts...)
+}
+
+// 导出所有任务配置选项
+var (
+	WithName            = scheduler.WithName
+	WithJob             = scheduler.WithJob
+	WithTimeout         = scheduler.WithTimeout
+	WithRepeat          = scheduler.WithRepeat
+	WithMaxRuns         = scheduler.WithMaxRuns
+	WithRetry           = scheduler.WithRetry
+	WithParallelism     = scheduler.WithParallelism
+	WithLogger          = scheduler.WithLogger
+	WithRecover         = scheduler.WithRecover
+	WithStartupDelay    = scheduler.WithStartupDelay
+	WithPreHook         = scheduler.WithPreHook
+	WithPostHook        = scheduler.WithPostHook
+	WithErrorHandler    = scheduler.WithErrorHandler
+	WithCancelOnFailure = scheduler.WithCancelOnFailure
+	WithMetricCollector = scheduler.WithMetricCollector
+	WithSchedule        = scheduler.WithSchedule
+	WithNaturalTime     = scheduler.WithNaturalTime
+)
