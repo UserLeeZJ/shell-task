be2111ab07d2977ccbdd212c932a59ff7f808421
UserLeeZJ <1072136039@qq.com>
2025-05-15 10:14:30 +0800

    fix example

--- examples/worker-pool-example/main.go
+++ examples/worker-pool-example/main.go
@@ -3,7 +3,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"math/rand"
 	"time"
@@ -18,7 +17,7 @@ func createTask(name string, priority task.Priority, duration time.Duration) *ta
 		task.WithPriority(priority),
 		task.WithJob(func(ctx context.Context) error {
 			log.Printf("开始执行任务: %s (优先级: %d, 预计耗时: %v)", name, priority, duration)
-			
+
 			// 模拟工作负载
 			select {
 			case <-ctx.Done():
@@ -29,32 +28,26 @@ func createTask(name string, priority task.Priority, duration time.Duration) *ta
 				return nil
 			}
 		}),
-		// 设置资源限制
-		task.WithResourceLimits(task.ResourceLimits{
-			MaxCPU:    int(priority) * 10, // 优先级越高，CPU 使用限制越高
-			MaxMemory: 100,                // 100MB 内存限制
-			MaxTime:   duration + time.Second, // 最大执行时间比预期多1秒
-		}),
 	)
 }
 
 func main() {
 	// 设置随机种子
 	rand.Seed(time.Now().UnixNano())
-	
+
 	// 设置日志格式
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 	log.Println("启动工作池示例...")
-	
+
 	// 创建一个自定义日志记录器
 	logger := task.NewFuncLogger(func(format string, args ...any) {
 		log.Printf("[WorkerPool] "+format, args...)
 	})
-	
+
 	// 创建工作池，限制最多3个并发任务
 	pool := task.NewWorkerPool(3, logger)
 	pool.Start()
-	
+
 	// 创建一些任务，具有不同的优先级
 	tasks := []*task.Task{
 		createTask("低优先级任务-1", task.PriorityLow, 3*time.Second),
@@ -67,18 +60,18 @@ func main() {
 		createTask("普通优先级任务-3", task.PriorityNormal, 1*time.Second),
 		createTask("高优先级任务-3", task.PriorityHigh, 3*time.Second),
 	}
-	
+
 	// 提交任务到工作池
 	for _, t := range tasks {
 		pool.Submit(t)
 		// 稍微延迟一下，便于观察
 		time.Sleep(100 * time.Millisecond)
 	}
-	
+
 	// 等待所有任务完成
 	log.Println("等待所有任务完成...")
 	time.Sleep(15 * time.Second)
-	
+
 	// 停止工作池
 	pool.Stop()
 	log.Println("工作池已停止，示例结束")
